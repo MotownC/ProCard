e78c95907a6145007d1815b659a77c1db4489cf5
MotownC <cander19@yahoo.com>
2025-12-10 16:34:40 -0500

    fix iOS flip icons7

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -390,7 +390,7 @@ const GalleryCard: React.FC<{
 
         {/* --- BACK FACE --- */}
         <div className="absolute inset-0 w-full h-full [backface-visibility:hidden] [transform:rotateY(180deg)] rounded-xl overflow-hidden border border-slate-700 shadow-xl bg-slate-800">
-            <div className="relative w-full h-full">
+            <div className="relative w-full h-full [transform:scaleX(-1)]">
                <div className="absolute top-3 left-3 z-20 p-1.5 bg-black/40 rounded-full opacity-0 group-hover:opacity-100 transition-opacity backdrop-blur-sm pointer-events-none">
                   <Maximize2 className="w-4 h-4 text-white" />
                </div>
